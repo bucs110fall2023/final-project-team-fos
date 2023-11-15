60acbd9f3078cbff0797dcb867b07272090d5dfd
Jae Woo Song <jsong20@binghamton.edu>
2023-11-14 22:57:11 -0500

    Team Full Of Stars

--- trial.py
+++ trial.py
@@ -4,10 +4,11 @@ pygame.init()
 screen = pygame.display.set_mode((600,600))
 
 a = 275
+x = 275
 
 hitboxes = {
-    "red" : pygame.Rect(a,0,50,50),
-    "white" : pygame.Rect(a,500,50,50)
+    "white" : pygame.Rect(x,500,50,50),
+    "red" : pygame.Rect(a,0,50,50)
 }
 
 main_colors = {
@@ -17,7 +18,7 @@ main_colors = {
 
 
 running = True
-times_used=[]
+
 
 while running:
     for event in pygame.event.get():
@@ -25,36 +26,38 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                screen.fill("black")
-                a = a - 20
-                pygame.draw.rect(screen, "white", (a,500, 50,50))
-                pygame.draw.rect(screen, "red", hitboxes["red"])
-                pygame.display.flip()
-            if event.key == pygame.K_RIGHT:
-                screen.fill("black")
-                a = a + 20
-                pygame.draw.rect(screen, "white", (a,500, 50,50))
-                pygame.draw.rect(screen, "red", hitboxes["red"])
-                pygame.display.flip()
-            if event.key == pygame.K_SPACE and times_used==[]:
+            if event.key == pygame.K_SPACE:
                 for color in hitboxes:
                     pygame.draw.rect(screen, main_colors[color], hitboxes[color])
                     pygame.display.flip()
-                    pygame.time.delay(1)
-                times_used.append(1)
-            if event.key == pygame.K_SPACE:
-                for n in range(9 ):
-                    b = 450-50*n
+            if event.key == pygame.K_LEFT:
+                screen.fill("black")
+                x = x-20
+                hitboxes["white"]= pygame.Rect(x,500,50,50)
+                pygame.draw.rect(screen, main_colors["white"], hitboxes["white"])
+                pygame.draw.rect(screen, main_colors["red"], hitboxes["red"])
+                pygame.display.flip()
+            if event.key == pygame.K_RIGHT:
+                screen.fill("black")
+                x = x+20
+                hitboxes["white"]= pygame.Rect(x,500,50,50)
+                pygame.draw.rect(screen, main_colors["white"], hitboxes["white"])
+                pygame.draw.rect(screen, main_colors["red"], hitboxes["red"])
+                pygame.display.flip()
+            if event.key == pygame.K_s:
+                for n in range(11):
+                    y = 450-50*n
                     screen.fill("black")
-                    pygame.draw.rect(screen, "white", (a,500, 50,50))
-                    pygame.draw.rect(screen, "red", hitboxes["red"])
-                    pygame.draw.rect(screen , "green", (a+20, b, 10, 50))
+                    pygame.draw.rect(screen, main_colors["white"], hitboxes["white"])
+                    pygame.draw.rect(screen, main_colors["red"], hitboxes["red"])
+                    pygame.draw.rect(screen , "green", (x+20, y, 10, 50))
                     pygame.display.flip()
                     pygame.time.delay(70)
-                    if b == 50:
+                    if a <= x+20 <= a+50 and y == 50:
                         screen.fill("black")
-                        pygame.draw.rect(screen, "white", (a,500, 50,50))
+                        main_colors["red"]="black"
+                        pygame.draw.rect(screen, main_colors["white"], hitboxes["white"])
+                        pygame.draw.rect(screen, main_colors["red"], hitboxes["red"])
                         pygame.display.flip()
                         
                 
