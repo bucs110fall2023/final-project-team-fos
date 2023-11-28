97129bfd35ed9c6e36c09d2084609d2fb3ec3a29
Jae Woo Song <jsong20@binghamton.edu>
2023-11-27 22:07:46 -0500

    Team

--- src/player.py
+++ src/player.py
@@ -2,38 +2,43 @@ import pygame
 
 class Player:
     
-    def __init__(self, x= 275, y= 500, image= "assets/starship.png"):
+    def __init__(self, x=275, y=500, image="assets/starship.png"):
         self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         self.rect.x = x 
         self.rect.y = y
         self.speed = 20 
     
-    def shootinglaser(self, width, height):
-        self.width = width
-        self.height = height
-        
-    
-    
-                
+    def move(self, direction):
+        if direction == "left":
+            self.rect.x -= self.speed
+        elif direction == "right":
+            self.rect.x += self.speed
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((600,600))
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()  # Add a clock to control the frame rate
     player = Player(image="assets/starship.png")
-    screen.blit(player)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    player.rect.x -= player.speed
-                
-                if event.key == pygame.K_RIGHT:
-                    player.rect.x += player.speed
-                
-        pygame.display.flip()
+                    player.move("left")
+                elif event.key == pygame.K_RIGHT:
+                    player.move("right")
 
-main()
+        screen.fill((0, 0, 0))  # Fill the screen with a black color
+        screen.blit(player.image, player.rect)  # Draw the player on the screen
+        
+        pygame.display.flip()
+        clock.tick(30)  # Set the frame rate to 30 frames per second
+
+if __name__ == "__main__":
+    main()
+
     
         
