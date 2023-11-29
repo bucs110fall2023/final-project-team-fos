51547a966940ad62afb386a34d64ac6152419099
Jae Woo Song <jsong20@binghamton.edu>
2023-11-28 22:53:55 -0500

    Team FOS

--- src/player.py
+++ src/player.py
@@ -10,30 +10,30 @@ class Player:
         self.rect.y = y
         self.speed = 20 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    clock = pygame.time.Clock()  # Add a clock to control the frame rate
-    player = Player()
+# def main():
+#     pygame.init()
+#     screen = pygame.display.set_mode((600, 600))
+#     clock = pygame.time.Clock()  # Add a clock to control the frame rate
+#     player = Player()
     
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.rect.x -= player.speed
-                elif event.key == pygame.K_RIGHT:
-                    player.rect.x += player.speed
+#     while True:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_LEFT:
+#                     player.rect.x -= player.speed
+#                 elif event.key == pygame.K_RIGHT:
+#                     player.rect.x += player.speed
 
-        screen.fill("black")  # Fill the screen with a black color
-        screen.blit(player.surface_obj, player.rect)  # Draw the player on the screen
+#         screen.fill("black")  # Fill the screen with a black color
+#         screen.blit(player.surface_obj, player.rect)  # Draw the player on the screen
         
-        pygame.display.flip()
-        clock.tick(30)  # Set the frame rate to 30 frames per second
+#         pygame.display.flip()
+#         clock.tick(30)  # Set the frame rate to 30 frames per second
 
 
-main()
+# main()
 
     
         
