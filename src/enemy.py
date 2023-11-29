5a3141427c1ef9a909f3b2cea35ca51f2e18e9f8
sacchitagavane <sacchitga@gmail.com>
2023-11-29 15:01:35 -0500

    different

--- src/enemy.py
+++ src/enemy.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 class Enemy(pygame.sprite.Sprite):
     
     def __init__(self, x=275, y=0):
@@ -9,3 +11,49 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.surface_obj.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()  # Add a clock to control the frame rate
+    enemy = Enemy()
+    enemies = pygame.sprite.Group()
+    number_of_times_2 = []
+    number_of_times_3 = []
+    current_time =0
+    for n in range(2):
+        enemies.add(Enemy(random.randint(0,575),-50))
+
+    
+#     while True:
+#         current_time = pygame.time.get_ticks()
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#         for enemy in enemies:
+#             enemy.rect.y = enemy.rect.y + 10
+#         screen.fill("black")  # Fill the screen with a black color
+#         for enemy in enemies:
+#             screen.blit(enemy.surface_obj, enemy.rect)  # Draw the player on the screen
+#         for enemy in enemies:
+#             if enemy.rect.y == 500:
+#                 for n in range(1):
+#                     enemies.add(Enemy(random.randint(0,575),0))
+#             if enemy.rect.y == 650:
+#                 enemy.kill() 
+                
+#         if 10000 < current_time <= 20000 and number_of_times_2==[] :
+#             for n in range(2):
+#                 enemies.add(Enemy(random.randint(0,575),-50))
+#             number_of_times_2.append(1)
+        
+#         if  20000 < current_time <= 30000 and number_of_times_3==[] :
+#             for n in range(2):
+#                 enemies.add(Enemy(random.randint(0,575),-50))
+#             number_of_times_3.append(1)
+                
+#         pygame.display.flip()
+#         clock.tick(10)  # Set the frame rate to 30 frames per second
+
+
+# main()
--- src/player.py
+++ src/player.py
@@ -1,47 +1,36 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((600, 600))
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x=275, y=500, width=50, height=50, image_path="assets/starship.png"):
-        super().__init__()
-        original_image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(original_image, (width, height))  # Resize the image
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 5
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 5
-
-def main():
-    player = Player(width=70, height=70)  # Set the desired width and height
-    all_sprites = pygame.sprite.Group(player)
-
-    clock = pygame.time.Clock()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        all_sprites.update()
-
-        screen.fill('black')  
-        # Draw all sprites, including the player's image
-        all_sprites.draw(screen)
-
-        pygame.display.flip()
-        clock.tick(30)  # Limit frames per second
-
-    pygame.quit()
+class Player:
     
+    def __init__(self, x=275, y=500):
+        self.surface_obj = pygame.Surface((50,50))
+        self.surface_obj.fill("white")
+        self.rect = self.surface_obj.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        self.speed = 20 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     pygame.init()
+#     screen = pygame.display.set_mode((600, 600))
+#     clock = pygame.time.Clock()  # Add a clock to control the frame rate
+#     player = Player()
+    
+#     while True:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_LEFT:
+#                     player.rect.x -= player.speed
+#                 elif event.key == pygame.K_RIGHT:
+#                     player.rect.x += player.speed
+
+#         screen.fill("black")  # Fill the screen with a black color
+#         screen.blit(player.surface_obj, player.rect)  # Draw the player on the screen
+        
+#         pygame.display.flip()
+#         clock.tick(30)  # Set the frame rate to 30 frames per second
+
+
+# main()
