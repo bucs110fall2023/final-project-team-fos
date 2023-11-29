4d588447aeae36360662099eb7bcf1ca4a458dc5
Jae Woo Song <jsong20@binghamton.edu>
2023-11-28 19:00:28 -0500

    Team fos

--- src/enemy.py
+++ src/enemy.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 class Enemy(pygame.sprite.Sprite):
     
     def __init__(self, x=275, y=0):
@@ -9,5 +11,28 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.surface_obj.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()  # Add a clock to control the frame rate
+    enemy = Enemy()
+    enemies = pygame.sprite.Group()
+    for n in range(random.randint(1,5)):
+        enemies.add(Enemy(random.randint(0,575),random.randint(-400,-50)))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        for enemy in enemies:
+            enemy.rect.y = enemy.rect.y + 10
+        screen.fill("black")  # Fill the screen with a black color
+        for enemy in enemies:
+            screen.blit(enemy.surface_obj, enemy.rect)  # Draw the player on the screen
         
-        
+        pygame.display.flip()
+        clock.tick(30)  # Set the frame rate to 30 frames per second
+
+
+main()
--- src/player.py
+++ src/player.py
@@ -2,24 +2,19 @@ import pygame
 
 class Player:
     
-    def __init__(self, x=275, y=500, image="assets/starship.png"):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
+    def __init__(self, x=275, y=500):
+        self.surface_obj = pygame.Surface((50,50))
+        self.surface_obj.fill("white")
+        self.rect = self.surface_obj.get_rect()
         self.rect.x = x 
         self.rect.y = y
         self.speed = 20 
-    
-    def move(self, direction):
-        if direction == "left":
-            self.rect.x -= self.speed
-        elif direction == "right":
-            self.rect.x += self.speed
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
     clock = pygame.time.Clock()  # Add a clock to control the frame rate
-    player = Player(image="assets/starship.png")
+    player = Player()
     
     while True:
         for event in pygame.event.get():
@@ -27,18 +22,18 @@ def main():
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    player.move("left")
+                    player.rect.x -= player.speed
                 elif event.key == pygame.K_RIGHT:
-                    player.move("right")
+                    player.rect.x += player.speed
 
-        screen.fill((0, 0, 0))  # Fill the screen with a black color
-        screen.blit(player.image, player.rect)  # Draw the player on the screen
+        screen.fill("black")  # Fill the screen with a black color
+        screen.blit(player.surface_obj, player.rect)  # Draw the player on the screen
         
         pygame.display.flip()
         clock.tick(30)  # Set the frame rate to 30 frames per second
 
-if __name__ == "__main__":
-    main()
+
+main()
 
     
         
